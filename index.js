9f39394b62618f9c93351b9a8516b230130b5f09
Tran Tho Manh 21020081 <62951382+tranthomanh@users.noreply.github.com>
2023-11-07 13:12:51 +0700

    intial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+    var a = 1;
+    var b = 2;
+    res.send('Hello World');
+} )
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
